816d76065986030952e6067f60f56653ddd4b1f1
PapyrusThePlant <papyrustheplant@gmail.com>
2017-10-02 16:14:38 +0200

    Handle playlists and actually use downloaded songs.

--- music.py
+++ music.py
@@ -1,4 +1,5 @@
 import asyncio
+import functools
 import logging
 import os
 import pathlib
@@ -27,10 +28,19 @@ class MusicError(commands.UserInputError):
 
 
 class Song(discord.PCMVolumeTransformer):
+    def __init__(self, song_info):
+        self.info = song_info.info
+        self.requester = song_info.requester
+        self.channel = song_info.channel
+        self.filename = song_info.filename
+        super().__init__(discord.FFmpegPCMAudio(self.filename, before_options='-nostdin', options='-vn'))
+
+
+class SongInfo:
     ytdl_opts = {
         'default_search': 'auto',
         'format': 'bestaudio/best',
-        'ignoreerrors': False,
+        'ignoreerrors': True,
         'source_address': '0.0.0.0', # Make all connections via IPv4
         'nocheckcertificate': True,
         'restrictfilenames': True,
@@ -38,7 +48,8 @@ class Song(discord.PCMVolumeTransformer):
         'logtostderr': False,
         'no_warnings': True,
         'quiet': True,
-        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s'
+        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+        'noplaylist': True
     }
     ytdl = youtube_dl.YoutubeDL(ytdl_opts)
 
@@ -46,11 +57,8 @@ class Song(discord.PCMVolumeTransformer):
         self.info = info
         self.requester = requester
         self.channel = channel
-        self.filename = info.get('_filename', self.ytdl.prepare_filename(info))
-        source = info.get('url', info.get('file', None))
-        if not source:
-            raise MusicError('Source not found.')
-        super().__init__(discord.FFmpegPCMAudio(source, before_options='-nostdin', options='-vn'))
+        self.filename = info.get('_filename', self.ytdl.prepare_filename(self.info))
+        self.downloaded = asyncio.Event()
 
     @classmethod
     async def create(cls, query, requester, channel, loop=None):
@@ -62,26 +70,73 @@ class Song(discord.PCMVolumeTransformer):
         if is_file:
             return cls.from_file(query, requester, channel)
         else:
-            return await cls.from_ytdl(query, requester, channel, loop)
+            return await cls.from_ytdl(query, requester, channel, loop=loop)
 
     @classmethod
     def from_file(cls, file, requester, channel):
+        path = pathlib.Path(file)
+        if not path.exists():
+            raise MusicError(f'File {file} not found.')
+
         info = {
-            'file': file,
-            'title': pathlib.Path(file).stem,
+            '_filename': file,
+            'title': path.stem,
             'creator': 'local file',
         }
         return cls(info, requester, channel)
 
     @classmethod
-    async def from_ytdl(cls, url, requester, channel, loop=None):
+    async def from_ytdl(cls, request, requester, channel, loop=None):
         loop = loop or asyncio.get_event_loop()
-        info = await loop.run_in_executor(None, cls.ytdl.extract_info, url)
 
-        if "entries" in info:
-            info = info['entries'][0]
+        # Get sparse info about our query
+        partial = functools.partial(cls.ytdl.extract_info, request, download=False, process=False)
+        sparse_info = await loop.run_in_executor(None, partial)
+
+        if sparse_info is None:
+            raise MusicError(f'Could not retrieve info from input : {request}')
+
+        # If we get a playlist, select its first valid entry
+        if "entries" not in sparse_info:
+            info_to_process = sparse_info
+        else:
+            info_to_process = None
+            for entry in sparse_info['entries']:
+                if entry is not None:
+                    info_to_process = entry
+                    break
+            if info_to_process is None:
+                raise MusicError(f'Could not retrieve info from input : {request}')
+
+        # Process full video info
+        url = info_to_process.get('url', info_to_process.get('webpage_url', info_to_process.get('id')))
+        partial = functools.partial(cls.ytdl.extract_info, url, download=False)
+        processed_info = await loop.run_in_executor(None, partial)
+
+        if processed_info is None:
+            raise MusicError(f'Could not retrieve info from input : {request}')
+
+        # Select the first search result if any
+        if "entries" not in processed_info:
+            info = processed_info
+        else:
+            info = None
+            while info is None:
+                try:
+                    info = processed_info['entries'].pop(0)
+                except IndexError:
+                    raise MusicError(f'Could not retrieve info from url : {info_to_process["url"]}')
 
         return cls(info, requester, channel)
+
+    async def download(self, loop):
+        if not pathlib.Path(self.filename).exists():
+            partial = functools.partial(self.ytdl.extract_info, self.info['webpage_url'], download=True)
+            self.info = await loop.run_in_executor(None, partial)
+        self.downloaded.set()
+
+    async def wait_until_downloaded(self):
+        await self.downloaded.wait()
 
     def __str__(self):
         title = f"**{self.info['title']}**"
@@ -95,6 +150,11 @@ class Playlist(asyncio.Queue):
         return self._queue.__iter__()
 
     def clear(self):
+        for song in self._queue:
+            try:
+                os.remove(song.filename)
+            except:
+                pass
         self._queue.clear()
 
     def get_song(self):
@@ -159,10 +219,12 @@ class GuildMusicState:
         if self.playlist.empty():
             await self.stop()
         else:
-            next_song = self.playlist.get_song()
-            next_song.volume = self.player_volume
-            self.voice_client.play(next_song, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next_song(next_song, e), self.loop).result())
-            await next_song.channel.send(f'Now playing {next_song}')
+            next_song_info = self.playlist.get_song()
+            await next_song_info.wait_until_downloaded()
+            source = Song(next_song_info)
+            source.volume = self.player_volume
+            self.voice_client.play(source, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next_song(next_song_info, e), self.loop).result())
+            await next_song_info.channel.send(f'Now playing {next_song_info}')
 
 
 class Music:
@@ -226,7 +288,7 @@ class Music:
             ctx.music_state.voice_client = await destination.connect()
 
     @commands.command()
-    async def play(self, ctx, *, song: str):
+    async def play(self, ctx, *, request: str):
         """Plays a song or adds it to the playlist.
 
         Automatically searches with youtube_dl
@@ -235,22 +297,27 @@ class Music:
         """
         await ctx.message.add_reaction('\N{HOURGLASS}')
 
-        # Add the song to the playlist
-        source = await Song.create(song, ctx.author, ctx.channel, loop=ctx.bot.loop)
-        try:
-            ctx.music_state.playlist.add_song(source)
-        except asyncio.QueueFull:
-            raise MusicError('Playlist is full, try again later.')
+        # Create the SongInfo
+        song = await SongInfo.create(request, ctx.author, ctx.channel, loop=ctx.bot.loop)
 
         # Connect to the voice channel if needed
         if ctx.voice_client is None or not ctx.voice_client.is_connected():
             await ctx.invoke(self.join)
 
-        # Start playing or notify it's been added to the playlist
+        # Add the info to the playlist
+        try:
+            ctx.music_state.playlist.add_song(song)
+        except asyncio.QueueFull:
+            raise MusicError('Playlist is full, try again later.')
+
         if not ctx.music_state.is_playing():
+            # Download the song and play it
+            await song.download(ctx.bot.loop)
             await ctx.music_state.play_next_song()
         else:
-            await ctx.send(f'Queued {source} in position **#{ctx.music_state.playlist.qsize()}**')
+            # Schedule the song's download
+            ctx.bot.loop.create_task(song.download(ctx.bot.loop))
+            await ctx.send(f'Queued {song} in position **#{ctx.music_state.playlist.qsize()}**')
 
         await ctx.message.remove_reaction('\N{HOURGLASS}', ctx.me)
         await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
