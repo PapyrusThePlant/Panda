c6ea21629bf9f9a94415be8bc59c30a6efd13e51
PapyrusThePlant <papyrustheplant@gmail.com>
2020-03-17 22:45:33 +0100

    Migrate from pipenv to venv.

--- run_linux_mac.sh
+++ run_linux_mac.sh
@@ -1,50 +1,47 @@
 #!/usr/bin/env bash
 
-export PIPENV_VENV_IN_PROJECT=1
-
-git --version > /dev/null 2>&1
-if [ $? != 0 ]
+# Check git version
+if ! git --version > /dev/null 2>&1
 then
-    echo Cannot find Git executable, make sure it is installed and added to your PATH.
-    read -p "Press [Enter] to exit..."
-    exit 0
+  echo Cannot find Git executable, make sure it is installed and added to your PATH.
+  read -rp "Press [Enter] to exit..."
+  exit 0
 fi
 
-python3 --version > /dev/null 2>&1
-if [ $? != 0 ]
+# Check python version
+python_version=$(python3 --version)
+if [[ -z "$python_version" ]]
 then
-    echo Cannot find Python3 executable, make sure it is installed and added to your PATH.
-    read -p "Press [Enter] to exit..."
-    exit 0
+  echo Cannot find Python3 executable, make sure it is installed and added to your PATH.
+  read -rp "Press [Enter] to exit..."
+  exit 0
+elif printf '%s\n%s' "$python_version" "Python 3.6" | sort -CV
+then
+  echo Python 3.6 or newer required. "$python_version" found.
+  read -rp "Press [Enter] to exit..."
+  exit 0
 fi
 
-python3 -c "import pipenv" > /dev/null 2>&1
-if [ $? != 0 ]
+# Create the virtual env if necessary
+if [[ ! -d '.venv' ]]
 then
-    python3 -m pip install --user pipenv
+    python3 -m venv .venv
 fi
 
-pipenv --version > /dev/null 2>&1
-if [ $? != 0 ]
-then
-    pipenv_path=$(python3 -m site --user-base)/bin
-    PATH+=:$pipenv_path
-fi
+# Activate and update the virtual env
+source .venv/bin/activate
+pip install -Ur requirements.txt
 
-if [ ! -d '.venv' ]
+# Check libopus
+if ! python3 -c "exit(__import__('discord').opus.is_loaded())" > /dev/null 2>&1
 then
-    pipenv --bare install
-else
-    pipenv --bare update
-fi
-
-pipenv run python3 -c "exit(__import__('discord').opus.is_loaded())" > /dev/null 2>&1
-if [ $? == 0 ]
-then
-    echo Cannot find libopus on your system, make sure it is installed.
-    read -p "Press [Enter] to exit..."
-    exit 0
+  echo Cannot find libopus on your system, make sure it is installed.
+  read -rp "Press [Enter] to exit..."
+  exit 0
 fi
 
 rm panda.log
-pipenv run python3 panda.py
+python3 panda.py
+
+# Deactivate the virtual env
+deactivate
--- run_windows.bat
+++ run_windows.bat
@@ -1,9 +1,7 @@
 @echo off
-SETLOCAL EnableDelayedExpansion
 chcp 65001 > NUL
 
-set PIPENV_VENV_IN_PROJECT=1
-
+REM Check git version
 git --version > NUL 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Cannot find Git executable, make sure it is installed and added to your PATH.
@@ -11,28 +9,33 @@ if %ERRORLEVEL% neq 0 (
     exit /B 0
 )
 
-python --version > NUL 2>&1
-if %ERRORLEVEL% neq 0 (
+REM Check python version
+for /f "delims= " %%i in ('python --version') do set python_version=%%i
+if not defined python_version (
     echo Cannot find Python executable, make sure it is installed and added to your PATH.
     pause
     exit /B 0
+) else (
+    call :compareVersions %python_version:Python =% "3.6"
+    if %errorlevel% equ -1 (
+        echo Python 3.6 or newer required. %python_version% found.
+        pause
+        exit /B 0
+    )
 )
 
-python -c "import pipenv" > NUL 2>&1
-if %ERRORLEVEL% neq 0 (
-    python -m pip install --user pipenv
+REM Create the virtual env if necessary
+if not exist ".venv" (
+    echo Creating virtual env
+	python -m venv .venv
 )
 
-pipenv --version > NUL 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo Adding pipenv to PATH...
-    for /F %%p IN ('python -m site --user-site') do set path_pipenv=%%p
-    set PATH=!PATH!;!path_pipenv:site-packages=Scripts!
-)
+REM Activate and update the virtual env
+call .venv\Scripts\activate.bat
+pip install -Ur requirements.txt
 
-if not exist ".venv" ( pipenv --bare install ) else ( pipenv --bare update )
-
-pipenv run python3 -c "exit(__import__('discord').opus.is_loaded())" > NUL 2>&1
+REM Check libopus
+python3 -c "exit(__import__('discord').opus.is_loaded())" > NUL 2>&1
 if %ERRORLEVEL% equ 0 (
     echo Cannot find libopus on your system, make sure it is installed.
     pause
@@ -40,5 +43,51 @@ if %ERRORLEVEL% equ 0 (
 )
 
 del /Q panda.log
-pipenv run python panda.py
+python panda.py
+
+REM Deactivate the virtual env
+deactivate
 pause
+exit /b 0
+
+
+:compareVersions  version1  version2
+::
+:: Compares two version numbers and returns the result in the ERRORLEVEL
+::
+:: Returns 1 if version1 > version2
+::         0 if version1 = version2
+::        -1 if version1 < version2
+::
+:: The nodes must be delimited by . or , or -
+::
+:: Nodes are normally strictly numeric, without a 0 prefix. A letter suffix
+:: is treated as a separate node
+::
+setlocal enableDelayedExpansion
+set "v1=%~1"
+set "v2=%~2"
+call :divideLetters v1
+call :divideLetters v2
+:loop
+call :parseNode "%v1%" n1 v1
+call :parseNode "%v2%" n2 v2
+if %n1% gtr %n2% exit /b 1
+if %n1% lss %n2% exit /b -1
+if not defined v1 if not defined v2 exit /b 0
+if not defined v1 exit /b -1
+if not defined v2 exit /b 1
+goto :loop
+
+
+:parseNode  version  nodeVar  remainderVar
+for /f "tokens=1* delims=.,-" %%A in ("%~1") do (
+  set "%~2=%%A"
+  set "%~3=%%B"
+)
+exit /b
+
+
+:divideLetters  versionVar
+for %%C in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do set "%~1=!%~1:%%C=.%%C!"
+exit /b
