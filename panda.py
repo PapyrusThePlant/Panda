f83a5018693dfd1e4bcecc283076b0f919366012
PapyrusThePlant <papyrustheplant@gmail.com>
2020-03-25 15:34:56 +0100

    Add the `info` command.

--- panda.py
+++ panda.py
@@ -1,6 +1,10 @@
 import json
 import logging
+import os
+import psutil
+import time
 
+import discord
 import discord.ext.commands as commands
 
 # Setup logging
@@ -22,6 +26,37 @@ bot = commands.Bot(commands.when_mentioned_or(conf['prefix']), description='Neve
 # Load cogs
 for cog_name in conf['extensions']:
 	bot.load_extension(f'cogs.{cog_name}')
+
+start_time = time.time()
+
+
+def duration_to_str(duration):
+	# Extract minutes, hours and days
+	minutes, seconds = divmod(duration, 60)
+	hours, minutes = divmod(minutes, 60)
+	days, hours = divmod(hours, 24)
+
+	# Create a fancy string
+	return f"{f'{days} days, ' if days > 0 else ''}{f'{hours} hours, ' if hours > 0 else ''}{f'{minutes} minutes, ' if minutes > 0 else ''}{seconds} seconds"
+
+
+@bot.command(aliases=['infos'])
+async def info(ctx):
+	with os.popen('git log --pretty=format:"[`%h`](https://github.com/PapyrusThePlant/MusicPanda/commit/%h) %s" -n 5') as fp:
+		latest_commits = fp.read().strip()
+
+	embed = discord.Embed(description='[Click here to get your own Panda!](https://github.com/PapyrusThePlant/MusicPanda)', colour=discord.Colour.blurple())
+	embed.set_thumbnail(url='https://raw.githubusercontent.com/PapyrusThePlant/MusicPanda/master/images/panda.jpg')
+	embed.set_author(name='Author : Papyrus#0095', icon_url='https://cdn.discordapp.com/avatars/145110704293281792/2775b3ee7b6a865722b3f6a27da8b14a.webp?size=1024')
+	embed.add_field(name='Command prefixes', value=f'`@{ctx.guild.me.display_name} `, `{conf["prefix"]}`', inline=False)
+	embed.add_field(name='CPU', value=f'{psutil.cpu_percent()}%')
+	embed.add_field(name='Memory', value=f'{psutil.Process().memory_full_info().uss / 1048576:.2f} Mb') # Expressed in bytes, turn to Mb and round to 2 decimals
+	embed.add_field(name='Uptime', value=duration_to_str(round(time.time() - start_time)))
+	embed.add_field(name='Latest changes', value=latest_commits, inline=False)
+	embed.add_field(name='\N{ZERO WIDTH SPACE}', value='For any question about the bot, announcements and an easy way to get in touch with the author, feel free to join the dedicated [discord server](https://discord.gg/AvAsTHW).')
+	embed.set_footer(text='Powered by discord.py', icon_url='http://i.imgur.com/5BFecvA.png')
+
+	await ctx.send(embed=embed)
 
 
 # Cogs management
