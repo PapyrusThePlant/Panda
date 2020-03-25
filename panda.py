ef3345e529848431534bda62691d4e100b72e692
PapyrusThePlant <papyrustheplant@gmail.com>
2020-03-25 15:33:28 +0100

    Add the cogs management commands.

--- panda.py
+++ panda.py
@@ -1,33 +1,76 @@
 import json
 import logging
+
 import discord.ext.commands as commands
 
 # Setup logging
 rlog = logging.getLogger()
 rlog.setLevel(logging.INFO)
-handler = logging.FileHandler('panda.log', encoding='utf-8')
+handler = logging.FileHandler('logs/panda.log', encoding='utf-8')
 handler.setFormatter(logging.Formatter('{asctime}:{levelname}:{name}:{message}', style='{'))
 rlog.addHandler(handler)
 
+logging.getLogger('discord').setLevel(logging.WARNING)
+
 # Get the token
-with open('conf.json') as fp:
-    conf = json.load(fp)
+with open('conf/panda.json') as fp:
+	conf = json.load(fp)
 
 # Complicated bot creation
 bot = commands.Bot(commands.when_mentioned_or(conf['prefix']), description='Never say no to Panda.')
-bot.load_extension('music')
 
-# For when the bot is shitting itself
+# Load cogs
+for cog_name in conf['extensions']:
+	bot.load_extension(f'cogs.{cog_name}')
+
+
+# Cogs management
+@bot.command()
+@commands.has_permissions(manage_guild=True)
+async def load(ctx, name):
+	"""Loads an extension
+
+	This command requires the Manage Server permission.
+	"""
+	try:
+		ctx.bot.load_extension(f'cogs.{name}')
+	except commands.ExtensionAlreadyLoaded:
+		await ctx.send(f'Extension {name} already loaded.')
+	except commands.ExtensionNotFound:
+		await ctx.send(f'Extension {name} not found.')
+	else:
+		conf['extensions'].append(name)
+		await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+
+
+@bot.command()
+@commands.has_permissions(manage_guild=True)
+async def unload(ctx, name):
+	"""Unloads an extension
+
+	This command requires the Manage Server permission.
+	"""
+	try:
+		ctx.bot.unload_extension(f'cogs.{name}')
+	except commands.ExtensionNotLoaded:
+		await ctx.send(f'Extension {name} not loaded.')
+	else:
+		conf['extensions'].remove(name)
+		await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+
+
 @bot.command()
 @commands.has_permissions(manage_guild=True)
 async def reload(ctx):
-    """Reloads the music module.
+	"""Reloads the bot's extensions.
 
-    This command requires the Manage Server permission.
-    """
-    ctx.bot.unload_extension('music')
-    ctx.bot.load_extension('music')
-    await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+	This command requires the Manage Server permission.
+	"""
+	for cog_name in conf['extensions']:
+		await ctx.invoke(load, cog_name)
+		await ctx.invoke(unload, cog_name)
+
+	await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
 
 # Let's rock ! (and roll, because panda are round and fluffy)
 bot.run(conf['token'])
